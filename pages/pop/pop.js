9928c68eb728cf1f9bd05e4c5da4b8f5ecbf88d5
Marvin Cao <caogang440@163.com>
2018-05-13 11:42:48 +0800

    modified:   app.json 	modified:   app.wxss 	modified:   pages/.DS_Store 	new file:   pages/pop/img/back.jpg 	new file:   pages/pop/img/pic.jpeg 	new file:   pages/pop/pop.js 	new file:   pages/pop/pop.json 	new file:   pages/pop/pop.wxml 	new file:   pages/pop/pop.wxss 	modified:   project.config.json

---
+++ pages/pop/pop.js
@@ -0,0 +1,25 @@
+// pop.js
+const app = getApp()
+
+Page({
+  data: {
+    flag: true,
+      },
+
+  /**
+   * 弹出层函数
+   */
+  //出现
+  show: function () {
+
+    this.setData({ flag: false })
+
+  },
+  //消失
+
+  hide: function () {
+
+    this.setData({ flag: true })
+
+  }
+})
